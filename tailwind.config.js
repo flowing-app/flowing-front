00c06f295129f42c54b09db72bf53f1c3633da40
yuta-ike <38308823+yuta-ike@users.noreply.github.com>
2023-08-11 19:55:41 +0900

    feat: tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,9 @@ module.exports = {
       fontFamily: {
         sans: "Helvetica Neue,Arial,Hiragino Kaku Gothic ProN,Hiragino Sans,Meiryo,sans-serif",
       },
+      data: {
+        active: "state='active'",
+      },
     },
   },
 }
