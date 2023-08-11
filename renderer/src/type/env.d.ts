d91afd67b762a1c3f90b9829dfce93f1eec0e6fa
yuta-ike <38308823+yuta-ike@users.noreply.github.com>
2023-08-11 19:55:20 +0900

    remove unused env vars

--- renderer/src/type/env.d.ts
+++ renderer/src/type/env.d.ts
@@ -3,7 +3,5 @@
 declare namespace NodeJS {
   interface ProcessEnv {
     readonly NODE_ENV: "development" | "production" | "test"
-    readonly ENV_TEST?: string
-    readonly NEXT_PUBLIC_API_BASE_URL?: string
   }
 }
